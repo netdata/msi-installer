5a5031bbd4f7e59635bc4684bb0340d45a076f85
Diego Pabon <diegofernandopabonrendon@hotmail.com>
2022-11-16 15:04:29 -0500

    MSI version from 'netdata -V' in x.x.x format

--- src/build_msi_with_wix_toolset.cmd
+++ src/build_msi_with_wix_toolset.cmd
@@ -1,2 +1,3 @@
-candle.exe -nologo netdata.wxs -out netdata.wixobj
+REM Legal version values should look like 'x.x.x.x'
+candle.exe -dProductVersion="1.36.0" -nologo netdata.wxs -out netdata.wixobj
 light.exe netdata.wixobj -spdb -out netdata.msi
